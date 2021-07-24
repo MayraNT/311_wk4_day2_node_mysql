af32cf39bfabaffb79c111ce802480a3336258ac
mayrant <mayrant2004@yahoo.com>
2021-07-23 23:06:22 -0500

    updated connection pool credentials

--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
+        host: '127.0.0.1',
         user: 'root',
-        password: 'password',
+        password: 'imaDEVgirl',
         database: 'admin'
       })
 
