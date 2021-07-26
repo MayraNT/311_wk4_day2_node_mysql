de4b9b7aaa0c12da6e7308af8b20ede51e30eedf
mayrant <mayrant2004@yahoo.com>
2021-07-26 00:04:25 -0500

    fixed getAllUsers, attempted full query for createUser

--- controllers/users.js
+++ controllers/users.js
@@ -4,8 +4,8 @@ const { handleSQLError } = require('../sql/error')
 
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
-  let sql = "select ??.*, ??.*, ??.* from ?? join ?? on ??.user_id = ??.id join ?? on ??.user_id = ??.id"
-  sql = mysql.format(sql, ["users", "usersContact", "usersAddress", "users", "usersContact", "usersContact", "users", "usersAddress", "usersAddress", "users"])
+  let sql = "select * from ??, ??, ?? where ??.user_id = ??.id and ??.user_id = ??.id"
+  sql = mysql.format(sql, ["users", "usersContact", "usersAddress", "usersContact", "users", "usersAddress", "users"])
 
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
@@ -33,6 +33,20 @@ const createUser = (req, res) => {
     if (err) return handleSQLError(res, err)
     return res.json({ newId: results.insertId });
   })
+
+  // I couldn't figure out insert into multiple tables. Below is my best attempt...
+  
+  // let sqlUser = "insert into ?? (??, ??) values (?, ?)"
+  // let sqlContact = "insert into ?? (??, ??, ??) values (?, ?, ?)"
+  // let sqlAddress = "insert into ?? (??, ??, ??, ??, ??) values (?, ?, ?, ?, ?)"
+  
+  // let valuesUser = ["users", "first_name", "last_name", req.body.first_name, req.body.last_name]
+  // let valuesContact = ["usersContact", "phone1", "phone2", "email", req.body.phone1, req.body.phone2, req.body.email]
+  // let valuesAddress = ["usersAddress", "address", "city", "county", "state", "zip", req.body.address, req.body.city, req.body.county, req.body.state, req.body.zip]
+
+  // sqlUser = mysql.format(sqlUser, valuesUser)
+  // sqlContact = mysql.format(sqlContact, valuesContact)
+  // sqlAddress = mysql.format(sqlAddress, valuesAddress)
 }
 
 const updateUserById = (req, res) => {
